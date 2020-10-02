66808eff8c346dcdff53afb472ea0632eb65ca0a
Nightscape Coffee <0c48d607@opayq.com>
2020-10-02 12:50:15 -0700

    initial gridsome

---
+++ gridsome/src/main.js
@@ -0,0 +1,62 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+import DefaultLayout from '~/layouts/Default.vue'
+import '~/assets/scss/globals.scss'
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import DefaultLayout from '~/layouts/Default.vue'
+require('typeface-source-sans-pro')
+
+export default function (Vue, { router, head, isClient, appOptions }) {
+  Vue.use(Vuex)
+
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout)
+
+  // Add attributes to HTML tag
+  head.htmlAttrs = { lang: 'en' }
+
+  head.link.push({
+    rel: 'manifest',
+    href: '/manifest.json'
+  })
+
+  head.meta.push({
+    name: 'theme-color',
+    content: '#10c186'
+  })
+
+  head.meta.push({
+    name: 'google-site-verification',
+    content: process.env.GSV_META
+  })
+
+  head.meta.push({
+    name: 'apple-mobile-web-app-status-bar-style',
+    content: 'default'
+  })
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css',
+  })
+  
+  const opts = { ... } //opts includes, vuetify themes, icons, etc.
+  Vue.use(Vuetify)
+  
+  appOptions.vuetify = new Vuetify(opts);
+  
+  // Set default layout as a global component
+  // Vue.component('Layout', DefaultLayout)
+
+  // State
+  appOptions.store = new Vuex.Store({
+    state: {
+      sidebarOpen: false
+    },
+    mutations: {
+    }
+  })
+}
---
+++ gridsome/src/pages/Index.vue
@@ -0,0 +1,79 @@
+<template>
+  <Layout>
+    <div class="content">
+      <h1>{{ $static.metadata.siteName }} - {{ this.description }}</h1>
+    </div>
+  </Layout>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+import Shortcut from '~/components/Shortcut.vue'
+
+export default {
+  components: {
+    Shortcut
+  },
+  data() {
+    return {
+      description: 'The ultimate static generated documentation theme for the JAM-stack'
+    }
+  },
+  metaInfo() {
+    return {
+      title: this.description,
+      meta: [
+        { key: 'description', name: 'description', content: 'A theme for static site documentation based on Gridsome, ready to deploy to Netlify in one click.' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+}
+
+h1 {
+  text-align: center;
+  max-width: 600px;
+  margin: 1.5em auto 1.5em;
+
+  @include respond-above(md) {
+    max-width: 1000px;
+  }
+}
+
+h2 {
+  font-size: 1.4rem;
+  margin: 0;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+
+  @include respond-above(sm) {
+    flex-direction: row;
+  }
+}
+
+.git {
+  margin: 3em 0 0;
+  align-self: center;
+
+  @include respond-above(md) {
+    margin: 5em 0 0;
+  }
+}
+</style>
